d57794e0edfd46814c1a4e61db87ff3e141f31f1
Abdullah Alhawsawi <>
2018-03-30 11:28:24 -0600

    updated this mongo shit

--- web-server.js
+++ web-server.js
@@ -2,13 +2,33 @@ var mongoose = require('mongoose');
 var express = require("express");
 var parser = require("body-parser");
 
-mongoose.connect('mongodb://aalha503:+eg89Jle@ds135983.mlab.com:35983/funwebdev');
+//connecting to mongo. Source: https://devcenter.heroku.com/articles/nodejs-mongoose
+var upstring = 
+process.env.MONGODB_URI ||
+'mongodb://localhost/HelloMongoose';
+
+//server will listen to appropriate port or default to port 5000
+var theport = process.env.PORT || 5000;
+
+// Makes connection asynchronously.  Mongoose will queue up database
+    // operations and release them when the connection is complete.
+    mongoose.connect(uristring, function (err, res) {
+      if (err) {
+      console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+      } else {
+      console.log ('Succeeded connected to: ' + uristring);
+      }
+    });
+
+/*
+						FROM NODE LAB 2
+mongoose.connect('mongodb://Abdul:Comp4513@ds125469.mlab.com:25469/heroku_ghq7zd4j');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error: ')); 
 db.once('open', function callback (){
     console.log("connected to mlab mongo");
 })
-
+*/
 var stockSchema = new mongoose.Schema(
     {
     symbol: String,
@@ -29,7 +49,7 @@ var app = express();
 app.use(parser.json);
 app.use(parser.urlencoded({extended:true}));
 
-//returns all companies
+//returns a single stock
 app.route('api/companies/:symbol')
 .get(function(req, res){
     Stock.find({symbol: req.params.symbol}, function(error, data){
