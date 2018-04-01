4bc0560051502b03f8cdc67d2410026da9ee1c90
Abdullah Alhawsawi <>
2018-04-01 17:19:00 -0600

    test index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World\n');
     
-}).listen(8080, process.env.IP, function () {
+}).listen(process.env.PORT || 8080, function () {
     console.log("Running on", process.env.PORT + ":" + process.env.IP);
 });
